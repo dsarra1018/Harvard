7007d24fcfffe3cddcfb780e334b00982f7a7897
darren sarra <darrensarra@macbook-pro.mynetworksettings.com>
2022-02-15 22:14:35 -0500

    Updated week 3 lecture notes.

---
+++ C/lecture/week3/name.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string name[] = {"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"};
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (strcmp(name[i], "Ron") == 0)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
